db1da7916783070b87fca15c4f23e1e451d73734
Jack Jarvis <jack.jarvis@red-gate.com>
2021-07-02 17:53:48 +0100

    Tidy up square tests with react testing library

--- src/test/Square.test.tsx
+++ src/test/Square.test.tsx
@@ -1,39 +1,26 @@
-import { render } from "@testing-library/react";
-import { unmountComponentAtNode } from "react-dom";
+import { fireEvent, render, screen } from "@testing-library/react";
+import "@testing-library/jest-dom/extend-expect";
 import { act } from "react-dom/test-utils";
 import { Square } from "../Square";
 import { SquareState } from "../Types";
 
-let container : Element = document.createElement("div");
-
-beforeEach(() => {
-    container = document.createElement("div");
-    document.body.appendChild(container);
-});
-
-afterEach(() => {
-    unmountComponentAtNode(container);
-    container.remove();
-});
-
 const possibleSquareStates: SquareState[] = ["", "O", "X"];
 
-describe("square", () => {
-    it.each(possibleSquareStates)("should display %p correctly", (symbol) => {
-        act(() => {
-            render(<Square value={symbol} onClick={()=>{}}/>, container);
-        });
-        expect(document.querySelector("button")?.textContent).toBe(symbol);
+describe("Square", () => {
+  it.each(possibleSquareStates)("should display %p correctly", (symbol) => {
+    act(() => {
+      render(<Square value={symbol} onClick={() => {}} />);
+    });
+    expect(screen.getByRole("button")).toHaveTextContent(symbol);
+  });
+
+  it("should call onClick when clicked", () => {
+    const onClick = jest.fn();
+    act(() => {
+      render(<Square value="" onClick={onClick} />);
+      fireEvent.click(screen.getByRole("button"));
     });
 
-    it("should call onClick when clicked", () => {
-        const onClick = jest.fn();
-        act(() => {
-            render(<Square value="" onClick={onClick}/>, container);
-            const button = document.querySelector("button");
-            button?.dispatchEvent(new MouseEvent("click", {bubbles: true}))
-        });
-
-        expect(onClick).toHaveBeenCalled();
-    });
+    expect(onClick).toHaveBeenCalled();
+  });
 });
